9fbcc1c806772d69044a799aaa1043ef4fce26a9
samuelkenenisa1-cyber <samuelkenenisa1@gmail.com>
2026-01-30 00:03:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,10 +135,15 @@ async def handle_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # ======================
 
 def main():
-    app = Application.builder().token(BOT_TOKEN).build()
-    app.add_handler(MessageHandler(filters.Document.PDF, handle_pdf))
-    print("🤖 Fayda bot running...")
-    app.run_polling()
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-if __name__ == "__main__":
-    main()
+    if not BOT_TOKEN:
+        raise RuntimeError("BOT_TOKEN is not set")
+
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.Document.PDF, handle_pdf))
+
+    print("🤖 Bot started...")
+    app.run_polling()
